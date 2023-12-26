9c8588a0e1a82b74797b98991876345b8f054c1e
Abhinav <r.abhinav413@gmail.com>
2023-12-26 22:18:04 +0530

    First commit

---
+++ src/main/java/org/example/introtooops/Main.java
@@ -0,0 +1,15 @@
+package org.example.introtooops;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student=new Student();
+
+        student.name="Abhinav";
+        student.age=25;
+        student.gender="Male";
+        student.batchName="CSE";
+
+        Student student2= new Student();
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/org/example/introtooops/Student.java
@@ -0,0 +1,19 @@
+package org.example.introtooops;
+
+public class Student {
+
+    String name;
+    int age;
+    String gender;
+
+    String batchName;
+
+    void changeBatch() {
+        System.out.println("Student is changing Batch");
+    }
+
+    void coursePause(){
+        System.out.println("Student is taking a course pause");
+    }
+
+}
